8e0dcfebd218934f39f63398ff3a8492e4fd9264
Baudino Costanza <baudino@INTRANET.EPFL.CH@SVUbuUniv20.intranet.epfl.ch>
2019-11-09 17:40:38 +0100

    Set PWM and Length

--- src/motif.cpp
+++ src/motif.cpp
@@ -1,2 +1,16 @@
 #include "motif.h"
+#include <iostream>
 
+void Motif::set_PWM(const std::array<double,4>& pos_motif) {
+	PWM.push_back(pos_motif);
+}
+
+void Motif::set_length(const double& lenght) {
+	Length=length;
+}
+
+/*
+void Motif::set_threshold(const double& threshold) {
+	Threshold=threshold;
+}
+*/
