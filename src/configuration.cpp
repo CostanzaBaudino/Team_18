7379fabcbef49c45e34d68bca1979df9d3421c32
Baudino Costanza <baudino@INTRANET.EPFL.CH@SVUbuUniv20.intranet.epfl.ch>
2019-11-09 17:30:02 +0100

    Lecture matrice

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -3,14 +3,55 @@
 void Configuration::parse(int argc, char **argv) {
    
     //tclap
-    //xoradd fichierF matriceM mutuellement exlusifs?
+    /* TCLAP::ValueArg<double> threshold("t", "threshold","Seuil score de vraisemblance" ,  false, 0, "double");
+    cmd.add(threshold);
+    xoradd fichierF matriceM mutuellement exlusifs?
+    TCLAP::ValueArg<std::string> matriceM ("m", "matrice", "Matrice file name", false, "", "string");  //sans switcharg
+    cmd.add(cfile);
     
     cmd.parse(argc, argv);
+    */
+ 
+
 
     //load_matrix ou load_positions
+    std::string conf(cfile.getValue());
+    if (conf.empty()) {
+    //lire fichierF et faire load_position maybe
+	} else {
+    load_matrix(conf)
+    //motiF.set_threshold(threshold.getvalue());
+      }
+ 
 }
 
-void Configuration::load_matrix(const std::string &infile);//counts the lines to get motif length
+void Configuration::load_matrix(const std::string &infile) {//counts the lines to get motif length
+   double length_motif(0.0);
+   try {
+        std::ifstream confstr(infile);
+        if (confstr.is_open()) {
+            std::string line,value;
+            std::array<double, 4> pos_motif;
+            
+            while(std::getline(confstr, line)) {
+				std::stringstream ss(line);
+				
+				while (std::getline(ss,value,' ')) {
+				pos_motif.push_back(stod(value));
+				}
+				motiF.set_PWM(pos_motif);
+				pos_motif.erase(pos_motif.begin(), pos_motif.end());
+				++length_motif;
+			}
+	motiF.set_length(length_motif);
+	confstr.close();
+        } else {
+            throw std::ifstream::failure ("Could not open configuration file " + infile);
+        }
+    } catch(std::ifstream::failure &e) {
+       std::cerr<<e.what() <<std::endl;
+    }
+}
 
 void Configuration::load_positions(const std::string &infile);
 
